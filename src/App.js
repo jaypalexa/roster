8c6d32ea520f47f2ea958cb1348f40377f7869f7
JP Alexander <jpalexa@gmail.com>
2019-10-05 11:51:31 +0100

    Added ROSTER banner

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
+import banner from './assets/images/roster-banner.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={banner} alt="banner" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ROSTER (Record Of Sea Turtles Electronic Reports) collects marine turtle and holding tank data required by the Florida Fish and Wildlife Conservation Commission (FWC).
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://turtlegeek.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TurtleGeek.com
         </a>
       </header>
     </div>
