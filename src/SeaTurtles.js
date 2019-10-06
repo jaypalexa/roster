6df7260db882dbc6f83017ffa5e7ee01c0fe2ec4
JP Alexander <jpalexa@gmail.com>
2019-10-06 11:37:24 +0100

    Rudimentary routing with @reach/router

---
+++ src/SeaTurtles.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from '@reach/router';
+import './SeaTurtles.css';
+
+function SeaTurtles() {
+  return (
+    <div className="SeaTurtles">
+      <header className="SeaTurtles-header">
+        {/* <img src={logo} className="SeaTurtles-logo" alt="logo" /> */}
+        <p>
+          Sea Turtles
+        </p>
+        <Link to="/">Home</Link>
+      </header>
+    </div>
+  );
+}
+
+export default SeaTurtles;
