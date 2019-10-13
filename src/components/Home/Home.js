524c77bc1b49b9a72c1e25112a0adb4f50a8b6ff
JP Alexander <jpalexa@gmail.com>
2019-10-13 11:30:25 +0100

    More wiring in of React Bulma Components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import Columns from 'react-bulma-components/lib/components/columns';
+import Tile from 'react-bulma-components/lib/components/tile';
 import './Home.sass';
 
 export default props => (
-  <div className='columns is-centered'>
-    <div className='column is-four-fifths has-text-centered'>
-      <div className='tile is-ancestor'>
-        <div className='tile is-6 is-parent is-vertical'>
-          <article className='tile is-child notification is-primary'>
+  <Columns className='is-centered'>
+    <Columns.Column className='is-four-fifths has-text-centered'>
+      <Tile kind='ancestor'>
+        <Tile size='{6}' vertical kind='parent'>
+          <Tile renderAs='article' kind='child' notification color='primary'>
             <p className='title'>Sea Turtles</p>
             <Link to='/sea-turtles'>Sea Turtles</Link>
-          </article>
-          <article className='tile is-child notification is-warning'>
+          </Tile>
+          <Tile renderAs='article' kind='child' notification color='warning'>
             <p className='title'>Hatchling Events</p>
             <Link to='/sea-turtles'>Hatchling Events</Link>
-          </article>
-        </div>
-        <div className='tile is-6 is-parent is-vertical'>
-          <article className='tile is-child notification is-danger'>
+          </Tile>
+        </Tile>
+        <Tile size='{6}' vertical kind='parent'>
+          <Tile renderAs='article' kind='child' notification color='danger'>
             <p className='title'>Holding Tanks</p>
             <Link to='/sea-turtles'>Holding Tanks</Link>
-          </article>
-          <article className='tile is-child notification'>
+          </Tile>
+          <Tile renderAs='article' kind='child' notification>
             <p className='title'>Organization</p>
             <Link to='/sea-turtles'>Organization</Link>
-          </article>
-        </div>
-      </div>
+          </Tile>
+        </Tile>
+      </Tile>
       {props.children}
-    </div>
-  </div>
+    </Columns.Column>
+  </Columns>
 );
