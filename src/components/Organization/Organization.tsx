fd7589d80a842180ec955143dfb987ee70256c02
JP Alexander <jpalexa@gmail.com>
2020-01-06 21:42:02 +0000

    Organization form save/reset, US states dropdown, city, ZIP Code

--- src/components/Organization/Organization.tsx
+++ src/components/Organization/Organization.tsx
@@ -4,27 +4,94 @@ import './Organization.sass';
 
 const Organization: React.FC = () => {
 
+  const states = [
+    { value: '', name: '' },
+    { value: 'AK', name: 'AK' },
+    { value: 'AL', name: 'AL' },
+    { value: 'AR', name: 'AR' },
+    { value: 'AZ', name: 'AZ' },
+    { value: 'CA', name: 'CA' },
+    { value: 'CO', name: 'CO' },
+    { value: 'CT', name: 'CT' },
+    { value: 'DC', name: 'DC' },
+    { value: 'DE', name: 'DE' },
+    { value: 'FL', name: 'FL' },
+    { value: 'GA', name: 'GA' },
+    { value: 'HI', name: 'HI' },
+    { value: 'IA', name: 'IA' },
+    { value: 'ID', name: 'ID' },
+    { value: 'IL', name: 'IL' },
+    { value: 'IN', name: 'IN' },
+    { value: 'KS', name: 'KS' },
+    { value: 'KY', name: 'KY' },
+    { value: 'LA', name: 'LA' },
+    { value: 'MA', name: 'MA' },
+    { value: 'MD', name: 'MD' },
+    { value: 'ME', name: 'ME' },
+    { value: 'MI', name: 'MI' },
+    { value: 'MN', name: 'MN' },
+    { value: 'MO', name: 'MO' },
+    { value: 'MS', name: 'MS' },
+    { value: 'MT', name: 'MT' },
+    { value: 'NC', name: 'NC' },
+    { value: 'ND', name: 'ND' },
+    { value: 'NE', name: 'NE' },
+    { value: 'NH', name: 'NH' },
+    { value: 'NJ', name: 'NJ' },
+    { value: 'NM', name: 'NM' },
+    { value: 'NV', name: 'NV' },
+    { value: 'NY', name: 'NY' },
+    { value: 'OH', name: 'OH' },
+    { value: 'OK', name: 'OK' },
+    { value: 'OR', name: 'OR' },
+    { value: 'PA', name: 'PA' },
+    { value: 'RI', name: 'RI' },
+    { value: 'SC', name: 'SC' },
+    { value: 'SD', name: 'SD' },
+    { value: 'TN', name: 'TN' },
+    { value: 'TX', name: 'TX' },
+    { value: 'UT', name: 'UT' },
+    { value: 'VA', name: 'VA' },
+    { value: 'VT', name: 'VT' },
+    { value: 'WA', name: 'WA' },
+    { value: 'WI', name: 'WI' },
+    { value: 'WV', name: 'WV' },
+    { value: 'WY', name: 'WY' }         
+  ];
+
   type FormData = {
     organizationName: string;
     addressLine1: string;
     addressLine2: string;
+    city: string;
+    state: string;
+    zipCode: string;
   };
 
-  const defaultValues: FormData = {
-    organizationName: '',
-    addressLine1: '',
-    addressLine2: ''
-  };
+  // const defaultValues: FormData = {
+  //   organizationName: '',
+  //   addressLine1: '',
+  //   addressLine2: ''
+  // };
+
+  let defaultValues = JSON.parse(localStorage.getItem('organization') || '{}');
 
   const methods = useForm<FormData>({
     defaultValues: defaultValues
   });
 
-  const { register, handleSubmit, errors, watch } = methods;
+  const { errors, handleSubmit, register, reset, watch } = methods;
 
   const onSubmit = handleSubmit((values: FormData) => {
-    console.log('values', values);
+    console.log('in handleSubmit(): values', values);
+    localStorage.setItem('organization', JSON.stringify(values));
+    defaultValues = {...values};
   });
+
+  const handleCancel = (): void => {
+    console.log('in handleCancel()...');
+    reset({ ...defaultValues });
+  };
 
   return (
     <div id='organization' className='container'>
@@ -47,7 +114,7 @@ const Organization: React.FC = () => {
             </div>
 
             <div className='field'>
-              <label className='label'>Address</label>
+              <label className='label'>Address / City / State / ZIP Code</label>
               <div className='control'>
                 <input name='addressLine1' className='input' type='text' placeholder='Address Line 1' ref={register} />
               </div>
@@ -63,18 +130,32 @@ const Organization: React.FC = () => {
               <div className='field-body'>
                 <div className='field'>
                   <div className='control is-expanded'>
-                    <input name='city' className='input' type='text' placeholder='City' />
+                    <input name='city' className='input' type='text' placeholder='City' ref={register} />
                   </div>
                 </div>
                 <div className='field'>
                   <div className='control is-expanded'>
-                    <input name='state' className='input' type='text' placeholder='State' />
+                    <div className='select is-fullwidth'>
+                      <select name='state' ref={register}>
+                          {states.map((e, key) => {
+                              return <option key={key} value={e.value}>{e.name}</option>;
+                          })}
+                      </select>
+                    </div>
                   </div>
                 </div>
                 <div className='field'>
                   <div className='control is-expanded'>
-                    <input name='zipCode' className='input' type='text' placeholder='ZIP Code' />
+                    <input 
+                      name='zipCode' 
+                      className='input' 
+                      type='text' 
+                      placeholder='ZIP Code' 
+                      maxLength={10}
+                      ref={register({maxLength: {value: 10, message:'ZIP Code cannot exceed 10 characters'}})} 
+                    />
                   </div>
+                  <p className='help has-text-danger'>{errors.zipCode && errors.zipCode.message}</p>
                 </div>
               </div>
             </div>
@@ -98,12 +179,16 @@ const Organization: React.FC = () => {
 
             <div className='field is-grouped is-grouped-right'>
               <p className='control'>
-                <input type='submit' className='button is-success is-fixed-width-medium' value='Save' />
+                <input 
+                  type='button' 
+                  className='button is-danger is-fixed-width-medium' 
+                  value='Cancel'
+                  onClick={() => handleCancel()}
+                />
               </p>
+
               <p className='control'>
-                <button className='button is-danger is-fixed-width-medium'>
-                  Cancel
-                </button>
+                <input type='submit' className='button is-success is-fixed-width-medium' value='Save' />
               </p>
             </div>
 
