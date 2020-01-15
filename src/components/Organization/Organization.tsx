3fad6383978aef59f8bce1a245f2473707ce0ae7
JP Alexander <jpalexa@gmail.com>
2020-01-15 20:49:45 +0000

    Split Organization into tabs; started hatchling and washback balances

--- src/components/Organization/Organization.tsx
+++ src/components/Organization/Organization.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import NavigationPrompt from "react-router-navigation-prompt";
+import NavigationPrompt from 'react-router-navigation-prompt';
 import { useAppContext } from '../../contexts/AppContext';
 import OrganizationService from '../../services/OrganizationService';
 import StatesService from '../../services/StatesService';
@@ -41,6 +42,39 @@ const Organization: React.FC = () => {
     reset(currentOrganization);
   };
 
+  let tabs = document.querySelectorAll('.tabs li');
+  let tabsContent = document.querySelectorAll('.tab-content');
+
+  let deactvateAllTabs = function () {
+    tabs.forEach(function (tab) {
+      tab.classList.remove('is-active');
+    });
+  };
+
+  let hideTabsContent = function () {
+    tabsContent.forEach(function (tabContent) {
+      tabContent.classList.remove('is-active');
+    });
+  };
+
+  let activateTabsContent = function (tab: Element) {
+    tabsContent[getIndex(tab)].classList.add('is-active');
+  };
+
+  let getIndex = function (el: Element) {
+    const nodes = Array.prototype.slice.call(el?.parentElement?.children);
+    return nodes.indexOf(el);
+  };
+
+  tabs.forEach(function (tab: Element) {
+    tab.addEventListener('click', function () {
+      deactvateAllTabs();
+      hideTabsContent();
+      tab.classList.add('is-active');
+      activateTabsContent(tab);
+    });
+  })
+
   return (
     <div id='organization'>
       <NavigationPrompt when={formState.dirty}>
@@ -62,137 +96,235 @@ const Organization: React.FC = () => {
           </div>
         )}
       </NavigationPrompt>
+
       <div className='columns is-centered'>
         <div className='column is-four-fifths'>
           <h1 className='title has-text-centered'>Organization</h1>
           <form onSubmit={onSubmit}>
 
-            <div className='field is-horizontal'>
-              <div className='field-body'>
-                <div className='field'>
-                  <label className='label'>Organization Name</label>
-                  <div className='control'>
-                    <input name='organizationName'
-                      className={`input ${!watch('organizationName') ? 'is-danger' : ''}`}
-                      type='text'
-                      placeholder='Organization Name'
-                      ref={register({required: 'Organization Name is required'})}
-                    />
-                  </div>
-                  <p className='help has-text-danger'>{errors.organizationName && errors.organizationName.message}</p>
-                </div>
-                <div className='field'>
-                  <label className='label'>Permit Number</label>
-                  <div className='control is-expanded'>
-                    <input name='permitNumber' className='input' type='text' placeholder='Permit Number' ref={register} />
-                  </div>
-                </div>
-                <div className='field'>
-                  <label className='label'>Contact Name</label>
-                  <div className='control is-expanded'>
-                    <input name='contactName' className='input' type='text' placeholder='Contact Name' ref={register} />
-                  </div>
-                </div>
-              </div>
+            <div className='tabs'>
+              <ul>
+                <li className='is-active'><a>General Information</a></li>
+                <li><a>Hatchling and Washback Starting Balances</a></li>
+                <li><a>Preferences</a></li>
+              </ul>
             </div>
 
-            <div className='field is-horizontal'>
-              <div className='field-body'>
-                <div className='field'>
-                  <label className='label'>Address</label>
-                  <div className='control is-expanded'>
-                    <input name='address1' className='input' type='text' placeholder='Address Line 1' ref={register} />
-                  </div>
-                </div>
-                <div className='field'>
-                  <label className='label'>&nbsp;</label>
-                  <div className='control is-expanded'>
-                    <input name='address2' className='input' type='text' placeholder='Address Line 2' ref={register} />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className='field is-horizontal'>
-              <div className='field-body'>
-                <div className='field'>
-                  <div className='control is-expanded'>
-                    <input name='city' className='input' type='text' placeholder='City' ref={register} />
-                  </div>
-                </div>
-                <div className='field'>
-                  <div className='control is-expanded'>
-                    <div className='select is-fullwidth'>
-                      <select name='state' ref={register}>
-                          {states.map((e, key) => {
-                              return <option key={key} value={e.value}>{e.name}</option>;
-                          })}
-                      </select>
+            <div>
+              <section className='tab-content is-active'> {/* General Information */}
+                <div className='field is-horizontal'>
+                  <div className='field-body'>
+                    <div className='field'>
+                      <label className='label'>Organization Name</label>
+                      <div className='control'>
+                        <input name='organizationName'
+                          className={`input ${!watch('organizationName') ? 'is-danger' : ''}`}
+                          type='text'
+                          placeholder='Organization Name'
+                          ref={register({required: 'Organization Name is required'})}
+                        />
+                      </div>
+                      <p className='help has-text-danger'>{errors.organizationName && errors.organizationName.message}</p>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Permit Number</label>
+                      <div className='control is-expanded'>
+                        <input name='permitNumber' className='input' type='text' placeholder='Permit Number' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Contact Name</label>
+                      <div className='control is-expanded'>
+                        <input name='contactName' className='input' type='text' placeholder='Contact Name' ref={register} />
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className='field'>
-                  <div className='control is-expanded'>
-                    <input 
-                      name='zipCode' 
-                      className='input' 
-                      type='text' 
-                      placeholder='ZIP Code' 
-                      maxLength={10}
-                      ref={register({maxLength: {value: 10, message:'ZIP Code cannot exceed 10 characters'}})} 
-                    />
-                  </div>
-                  <p className='help has-text-danger'>{errors.zipCode && errors.zipCode.message}</p>
-                </div>
-              </div>
-            </div>
 
-            <div className='field is-horizontal'>
-              <div className='field-body'>
-                <div className='field'>
-                  <label className='label'>Phone</label>
-                  <div className='control is-expanded'>
-                    <input name='phone' className='input' type='text' placeholder='Phone' ref={register} />
+                <div className='field is-horizontal'>
+                  <div className='field-body'>
+                    <div className='field'>
+                      <label className='label'>Address</label>
+                      <div className='control is-expanded'>
+                        <input name='address1' className='input' type='text' placeholder='Address Line 1' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>&nbsp;</label>
+                      <div className='control is-expanded'>
+                        <input name='address2' className='input' type='text' placeholder='Address Line 2' ref={register} />
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className='field'>
-                  <label className='label'>Fax</label>
-                  <div className='control is-expanded'>
-                    <input name='fax' className='input' type='text' placeholder='Fax' ref={register} />
-                  </div>
-                </div>
-                <div className='field'>
-                  <label className='label'>Email Address</label>
-                  <div className='control is-expanded'>
-                    <input 
-                      name='emailAddress' 
-                      className='input' 
-                      type='text' 
-                      placeholder='Email Address' 
-                      ref={register({ 
-                        pattern: { 
-                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 
-                          message: 'Invalid email address' 
-                        }})}
-                    />
-                  </div>
-                  <p className='help has-text-danger'>{errors.emailAddress && errors.emailAddress.message}</p>
-                </div>
-              </div>
-            </div>
 
-            <div className='field'>
-              <label className='label'>Units Type</label>
-              <div className='control'>
-                <label className='radio'>
-                  <input type='radio' name='preferredUnitsType' value='M' ref={register}/>
-                  Metric
-                </label>
-                <label className='radio'>
-                  <input type='radio' name='preferredUnitsType' value='I' ref={register}/>
-                  Imperial
-                </label>
-              </div>
+                <div className='field is-horizontal'>
+                  <div className='field-body'>
+                    <div className='field'>
+                      <div className='control is-expanded'>
+                        <input name='city' className='input' type='text' placeholder='City' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <div className='control is-expanded'>
+                        <div className='select is-fullwidth'>
+                          <select name='state' ref={register}>
+                              {states.map((e, key) => {
+                                  return <option key={key} value={e.value}>{e.name}</option>;
+                              })}
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <div className='control is-expanded'>
+                        <input 
+                          name='zipCode' 
+                          className='input' 
+                          type='text' 
+                          placeholder='ZIP Code' 
+                          maxLength={10}
+                          ref={register({maxLength: {value: 10, message:'ZIP Code cannot exceed 10 characters'}})} 
+                        />
+                      </div>
+                      <p className='help has-text-danger'>{errors.zipCode && errors.zipCode.message}</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='field is-horizontal'>
+                  <div className='field-body'>
+                    <div className='field'>
+                      <label className='label'>Phone</label>
+                      <div className='control is-expanded'>
+                        <input name='phone' className='input' type='text' placeholder='Phone' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Fax</label>
+                      <div className='control is-expanded'>
+                        <input name='fax' className='input' type='text' placeholder='Fax' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Email Address</label>
+                      <div className='control is-expanded'>
+                        <input 
+                          name='emailAddress' 
+                          className='input' 
+                          type='text' 
+                          placeholder='Email Address' 
+                          ref={register({ 
+                            pattern: { 
+                              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 
+                              message: 'Invalid email address' 
+                            }})}
+                        />
+                      </div>
+                      <p className='help has-text-danger'>{errors.emailAddress && errors.emailAddress.message}</p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+
+              <section className='tab-content'> {/* Hatchling and Washback Starting Balances */}
+                <div className='columns is-centered'>
+                  <div className='column is-half'>
+                    <h2 className='is-size-5 has-text-centered'>Hatchlings</h2>
+                    <div className='field'>
+                      <label className='label'>Balance As Of</label>
+                      <div className='control is-expanded'>
+                        <input name='hatchlingBalanceAsOfDate' className='input' type='date' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Loggerhead (Cc)</label>
+                      <div className='control is-expanded'>
+                        <input name='ccHatchlingStartingBalance' className='input' type='number' min='0' pattern='\d+' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Green (Cm)</label>
+                      <div className='control is-expanded'>
+                        <input name='cmHatchlingStartingBalance' className='input' type='number' min='0' pattern='\d+' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Leatherback (Dc)</label>
+                      <div className='control is-expanded'>
+                        <input name='dcHatchlingStartingBalance' className='input' type='number' min='0' pattern='\d+' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Other</label>
+                      <div className='control is-expanded'>
+                        <input name='otherHatchlingStartingBalance' className='input' type='number' min='0' pattern='\d+' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Unknown</label>
+                      <div className='control is-expanded'>
+                        <input name='unKnownHatchlingStartingBalance' className='input' type='number' min='0' pattern='\d+' ref={register} />
+                      </div>
+                    </div>
+                  </div>
+                  <div className='column is-half'>
+                    <h2 className='is-size-5 has-text-centered'>Washbacks</h2>
+                    <div className='field'>
+                      <label className='label'>Balance As Of</label>
+                      <div className='control is-expanded'>
+                        <input name='washbackBalanceAsOfDate' className='input' type='date' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Loggerhead (Cc)</label>
+                      <div className='control is-expanded'>
+                        <input name='ccWashbackStartingBalance' className='input' type='number' min='0' pattern='\d+' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Green (Cm)</label>
+                      <div className='control is-expanded'>
+                        <input name='cmWashbackStartingBalance' className='input' type='number' min='0' pattern='\d+' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Leatherback (Dc)</label>
+                      <div className='control is-expanded'>
+                        <input name='dcWashbackStartingBalance' className='input' type='number' min='0' pattern='\d+' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Other</label>
+                      <div className='control is-expanded'>
+                        <input name='otherWashbackStartingBalance' className='input' type='number' min='0' pattern='\d+' ref={register} />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>Unknown</label>
+                      <div className='control is-expanded'>
+                        <input name='unKnownWashbackStartingBalance' className='input' type='number' min='0' pattern='\d+' ref={register} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+
+              <section className='tab-content'> {/* Preferences */}
+                <div className='field'>
+                  <label className='label'>Units Type</label>
+                  <div className='control'>
+                    <label className='radio'>
+                      <input type='radio' name='preferredUnitsType' value='M' ref={register}/>
+                      Metric
+                    </label>
+                    <label className='radio'>
+                      <input type='radio' name='preferredUnitsType' value='I' ref={register}/>
+                      Imperial
+                    </label>
+                  </div>
+                </div>
+              </section>
             </div>
 
             <div className='field is-grouped is-grouped-right'>
--- src/components/Reports/Reports.tsx
+++ src/components/Reports/Reports.tsx
@@ -1,40 +1,14 @@
-// @ts-ignore
-// import { pdfform } from 'pdfform.js';
 import React from 'react';
-// @ts-ignore // could not find module xxx TS2307
-import testPdf from '../../assets/pdfs/test.pdf';
 import browserHistory from '../../browserHistory';
 import './Reports.sass';
-// const pdfform = require("./pdfform.minipdf.dist");
-// const pdfform = require("pdfform.js");
-// const pdfform = require('../../assets/scripts/pdfform.minipdf.dist.js');
 
 const Reports: React.FC = () => {
-
-  var oReq = new XMLHttpRequest();    
-  oReq.open('get', '../../assets/pdfs/test.pdf' , true);
-  oReq.responseType = 'blob';
-  oReq.onload = () => {
-      var blob = oReq.response;
-      var reader = new FileReader();
-      reader.onload = (ev) => {
-        const buffer = ev.target?.result;
-        // const field_specs = pdfform().list_fields(buffer);
-      };
-      reader.readAsArrayBuffer(blob);
-  };
-  oReq.send();
-
   return (
     <div id='reports'>
       <div className='columns'>
         <div className='column has-text-centered'>
           <h1 className='title has-text-centered'>Reports</h1>
           <button className='button is-dark' onClick={() => browserHistory.push('/')}>Home</button>
-
-          <div>
-            <a href={testPdf} target='_blank' rel='noopener noreferrer'>Download Pdf</a>
-          </div>
         </div>
       </div>
     </div>
