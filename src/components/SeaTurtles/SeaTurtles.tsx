0f2c9cccad6e22d7dd7485f55d1abddebf3e285a
JP Alexander <john.alexander@airsweb.com>
2020-02-09 15:55:53 +0000

    Still sorting out prompting for unsaved changes when navigating in form

--- src/components/SeaTurtles/SeaTurtles.tsx
+++ src/components/SeaTurtles/SeaTurtles.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { FormContext, useForm } from 'react-hook-form';
-import { toast } from 'react-toastify';
-import uuidv4 from 'uuid/v4';
-import { useAppContext } from '../../contexts/AppContext';
-import TabHelper from '../../helpers/TabHelper';
 import CodeListTableService, { CodeTableType } from '../../services/CodeTableListService';
-import SeaTurtleService from '../../services/SeaTurtleService';
-import NameValuePair from '../../types/NameValuePair';
-import SeaTurtleModel from '../../types/SeaTurtleModel';
 import FormFieldRow from '../FormFields/FormFieldRow';
 import ListFormField from '../FormFields/ListFormField';
+import NameValuePair from '../../types/NameValuePair';
+import React, { useEffect, useState } from 'react';
+import SeaTurtleModel from '../../types/SeaTurtleModel';
+import SeaTurtleService from '../../services/SeaTurtleService';
+import TabHelper from '../../helpers/TabHelper';
 import TextFormField from '../FormFields/TextFormField';
 import UnsavedChangesDialog from '../UnsavedChanges/UnsavedChangesDialog';
 import UnsavedChangesWhenLeavingPrompt from '../UnsavedChanges/UnsavedChangesWhenLeavingPrompt';
+import uuidv4 from 'uuid/v4';
+import { FormContext, useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { useAppContext } from '../../contexts/AppContext';
 import './SeaTurtles.sass';
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
@@ -30,8 +30,9 @@ const SeaTurtles: React.FC = () => {
   const [turtleStatuses, setTurtleStatuses] = useState([] as Array<NameValuePair>);
   const [isFormEnabled, setIsFormEnabled] = useState(false);
   const [showUnsavedChangesDialog, setShowUnsavedChangesDialog] = useState(false);
-  const [selectedTurtleId, setSelectedTurtleId] = useState('');
-  const [onUnsavedChangesCancel, setOnUnsavedChangesCancel] = useState(() => {});
+  const [onUnsavedChangesConfirm, setOnUnsavedChangesConfirm] = useState(() => { });
+  const [onUnsavedChangesCancel, setOnUnsavedChangesCancel] = useState(() => { });
+
   // console.log(JSON.stringify(formState));
   // console.log(JSON.stringify(methods.errors));
 
@@ -52,6 +53,7 @@ const SeaTurtles: React.FC = () => {
 
   const fetchSeaTurtle = (turtleId: string) => {
     // make async server request
+    console.log('In fetchSeaTurtle()', turtleId);
     const getSeaTurtle = async () => {
       const seaTurtle = await SeaTurtleService.getSeaTurtle(turtleId);
       reset(seaTurtle);
@@ -61,50 +63,62 @@ const SeaTurtles: React.FC = () => {
   };
 
   const onAddNewButtonClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    //TODO:  If form is dirty, prompt to save changes
-    setShowUnsavedChangesDialog(formState.dirty);
-    const seaTurtle = {} as SeaTurtleModel;
-    seaTurtle.turtleId = uuidv4();
-    reset(seaTurtle);
-    setCurrentSeaTurtle(seaTurtle);
-    setIsFormEnabled(true);
-  };
-  
-  const onTurtleRowClick = (event: React.MouseEvent<HTMLTableRowElement, MouseEvent>) => {
-    //TODO:  If form is dirty, prompt to save changes
-    setSelectedTurtleId(event.currentTarget.getAttribute('data-item') || '');
+    const handleEvent = () => {
+      const seaTurtle = {} as SeaTurtleModel;
+      seaTurtle.turtleId = uuidv4();
+      reset(seaTurtle);
+      setCurrentSeaTurtle(seaTurtle);
+      setIsFormEnabled(true);
+    };
 
-    if (formState.dirty) 
-    {
-      setShowUnsavedChangesDialog(true);
-      // onUnsavedChangesConfirm = () => {
-      //   console.log('In onUnsavedChangesConfirm()...');
-      //   onSubmit();
-      //   fetchSeaTurtle(selectedTurtleId);
-      //   setIsFormEnabled(true);
-      //   setShowUnsavedChangesDialog(false);
-      // }
-      setOnUnsavedChangesCancel(() => () => {
-        console.log('In onUnsavedChangesCancel(2)...');
-        // fetchSeaTurtle(selectedTurtleId);
-        // setIsFormEnabled(true);
+    if (formState.dirty) {
+      setOnUnsavedChangesConfirm(() => async () => {
+        await onSubmit();
+        handleEvent();
         setShowUnsavedChangesDialog(false);
       });
+      setOnUnsavedChangesCancel(() => () => {
+        handleEvent();
+        setShowUnsavedChangesDialog(false);
+      });
+      setShowUnsavedChangesDialog(true);
     } else {
-      fetchSeaTurtle(selectedTurtleId);
+      handleEvent();
+    }
+  };
+
+  const onTurtleRowClick = (event: React.MouseEvent<HTMLTableRowElement, MouseEvent>) => {
+    const turtleId = event.currentTarget.getAttribute('data-item') || '';
+    const handleEvent = () => {
+      fetchSeaTurtle(turtleId);
       setIsFormEnabled(true);
-      setShowUnsavedChangesDialog(false);
+    };
+
+    if (formState.dirty) {
+      setOnUnsavedChangesConfirm(() => async () => {
+        await onSubmit();
+        handleEvent();
+        setShowUnsavedChangesDialog(false);
+      });
+      setOnUnsavedChangesCancel(() => () => {
+        handleEvent();
+        setShowUnsavedChangesDialog(false);
+      });
+      setShowUnsavedChangesDialog(true);
+    } else {
+      handleEvent();
     }
   };
 
   const onSubmit = handleSubmit((modifiedSeaTurtle: SeaTurtleModel) => {
+    console.log('In onSubmit()', JSON.stringify(modifiedSeaTurtle));
     const patchedSeaTurtle = { ...currentSeaTurtle, ...modifiedSeaTurtle };
     SeaTurtleService.saveSeaTurtle(patchedSeaTurtle);
     reset(patchedSeaTurtle);
     setCurrentSeaTurtle(patchedSeaTurtle);
     const index = currentSeaTurtles.findIndex(x => x.turtleId === patchedSeaTurtle.turtleId);
     if (~index) {
-      currentSeaTurtles[index] = {...patchedSeaTurtle};
+      currentSeaTurtles[index] = { ...patchedSeaTurtle };
     } else {
       currentSeaTurtles.push(patchedSeaTurtle);
     }
@@ -115,21 +129,12 @@ const SeaTurtles: React.FC = () => {
     reset(currentSeaTurtle);
   };
 
-  let onUnsavedChangesConfirm = () => {
-    setShowUnsavedChangesDialog(false);
-  }
-
-  // let onUnsavedChangesCancel = () => {
-  //   console.log('In onUnsavedChangesCancel(1)...');
-  //   setShowUnsavedChangesDialog(false);
-  // }
-  
   new TabHelper().initialize();
 
   return (
     <div id='seaTurtle'>
       <UnsavedChangesWhenLeavingPrompt isDirty={formState.dirty} />
-      <UnsavedChangesDialog isActive={showUnsavedChangesDialog} onConfirm={onUnsavedChangesConfirm} onCancel={onUnsavedChangesCancel}/>
+      <UnsavedChangesDialog isActive={showUnsavedChangesDialog} onConfirm={onUnsavedChangesConfirm} onCancel={onUnsavedChangesCancel} />
       <div className='columns is-centered'>
         <div className='column is-four-fifths'>
           <h1 className='title has-text-centered'>Sea Turtles</h1>
@@ -189,7 +194,7 @@ const SeaTurtles: React.FC = () => {
                 <div>
                   <section className='tab-content is-active'> {/* General Information */}
                     <FormFieldRow>
-                      <TextFormField fieldName='turtleName' labelText='Name' validationOptions={{required: 'Name is required'}} />
+                      <TextFormField fieldName='turtleName' labelText='Name' validationOptions={{ required: 'Name is required' }} />
                       <TextFormField fieldName='sidNumber' labelText='SID Number' />
                       <TextFormField fieldName='strandingIdNumber' labelText='Stranding ID Number' />
                     </FormFieldRow>
@@ -228,7 +233,7 @@ const SeaTurtles: React.FC = () => {
                   </p>
 
                 </div>
-                </fieldset>
+              </fieldset>
             </form>
           </FormContext>
 
