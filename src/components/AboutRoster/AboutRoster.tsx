4f6039b9817838551642b21c3d71ea5b059e3da6
JP Alexander <jpalexa@gmail.com>
2020-05-22 08:59:26 +0100

    Updated npm packages

--- src/components/AboutRoster/AboutRoster.tsx
+++ src/components/AboutRoster/AboutRoster.tsx
@@ -110,7 +110,7 @@ const AboutRoster: React.FC = () => {
             <a href='https://github.com/jaypalexa/roster' target='_blank' rel='noopener noreferrer' title='GitHub'>
               GitHub
             </a>
-            &nbsp;|&nbsp;v0.20200521.2040
+            &nbsp;|&nbsp;v0.20200522.0900 
             {isShowUpdateAvailable ? <p><span>(</span><span className='span-link' onClick={onUpdateAvailableClick}>update available</span><span>)</span></p> : null}
             {!isShowUpdateAvailable ? <p><span>(</span><span className='span-link' onClick={onCheckForUpdateClick}>check for update</span>{lastUpdateCheckDateTime ? <span> - last checked: {lastUpdateCheckDateTime}</span> : null}<span>)</span></p> : null}
           </div>
