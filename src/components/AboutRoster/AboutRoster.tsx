b1524ee7a687ffcffbd434eb7bf95212a1c87c5b
JP Alexander <jpalexa@gmail.com>
2020-05-24 11:10:17 +0100

    Stub for Marine Turtle Holding Facility Quarterly Report handler

--- src/components/AboutRoster/AboutRoster.tsx
+++ src/components/AboutRoster/AboutRoster.tsx
@@ -109,7 +109,7 @@ const AboutRoster: React.FC = () => {
         <div className='column is-four-fifths'>
           <h1 className='title has-text-centered'>About ROSTER</h1>
           <div className='has-text-centered'>
-            <p>v0.20200523.1925</p>
+            <p>v0.20200524.1100</p>
             {isShowUpdateAvailable
               ? <p>
                   <span>(</span>
