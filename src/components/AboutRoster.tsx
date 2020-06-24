3e1027a74928e2afc14f9e82b8df585549dbe29f
JP Alexander <jpalexa@gmail.com>
2020-06-24 10:12:48 +0100

    Updated npm packages

--- src/components/AboutRoster.tsx
+++ src/components/AboutRoster.tsx
@@ -76,7 +76,7 @@ const AboutRoster: React.FC = () => {
         <Grid item xs={12} md={8}>
           <Typography variant='h1' align='center' gutterBottom={true}>About ROSTER</Typography>
           <Box textAlign='center'>
-            <p>v2020.06.23.0815</p>
+            <p>v2020.06.24.1000</p>
             {isUpdateAvailable
               ? <p>
                   <span>(</span>
