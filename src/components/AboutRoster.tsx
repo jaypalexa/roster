001274365c59f7be35826ec3bb39d35973110795
JP Alexander <jpalexa@gmail.com>
2020-07-06 11:12:55 +0100

    Updated npm packages; added eslint directory exclusions

--- src/components/AboutRoster.tsx
+++ src/components/AboutRoster.tsx
@@ -76,7 +76,7 @@ const AboutRoster: React.FC = () => {
         <Grid item xs={12} md={8}>
           <Typography variant='h1' align='center' gutterBottom={true}>About ROSTER</Typography>
           <Box textAlign='center'>
-            <p>v2020.07.03.1230</p>
+            <p>v2020.07.06.1100</p>
             {isUpdateAvailable
               ? <p>
                   <span>(</span>
